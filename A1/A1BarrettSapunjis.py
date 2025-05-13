0fc4e63b0e3fbd7b1e2c9f9eaeae8b2312d3d2b0
Barrett Sapunjis <barrettsapunjis@me.com>
2025-05-13 09:23:57 -0600

    commiting up to A1

---
+++ A1/A1BarrettSapunjis.py
@@ -0,0 +1,16 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Barrett Sapunjis
+
+studentName = "Barrett Sapunjis"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+
+
+message = "my ability to understand systems and break up a greater process into smaller components."
+
+print("My best skill in regards to engineering is " + message)
+
+
