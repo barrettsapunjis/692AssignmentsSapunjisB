f50a1d30f19bf5bb3690414a2c24cf36ca776ebc
Icy25x <barrettsapunjis@me.com>
2025-05-13 14:18:41 -0600

    Completed bulk of A2

--- A2/input_processing.py
+++ A2/input_processing.py
@@ -19,22 +19,32 @@ class Sensor:
     # You do not need to provide commenting above the constructor
     def __init__(self):
         self.status = { 
-            "Light": {"val": "", "Options": {"Red", "Yellow", "Green"}},
-            "Person": {"val": "", "Options": {"Yes", "No"}},
-            "Vehicle": {"val": "", "Options": {"Yes", "No"}},
+            "Light": {"val": "Green", "Options": {"Red", "Yellow", "Green"}},
+            "Person": {"val": "Yes", "Options": {"Yes", "No"}},
+            "Vehicle": {"val": "Yes", "Options": {"Yes", "No"}},
         }
 
         self.inputOptions = ["0", "1", "2", "3"]
+
+        self.rules = { 
+            ("Green", "No", "No"): "Go",
+            ("Yellow", "No", "No"): "Caution"
+        }
         pass
 
     # Replace these comments with your function commenting
     def update_status(self, param): # You may decide how to implement the arguments for this function
-        while(True): 
-            print("You have selected: " + param " please enter one of the follow options:" + huh)
+        while(True):
+            options = self.status[param]["Options"]
+            print(f"You have selected: {param}  please enter one of the follow options: {options}")
             userIn = input()
             if(self.checkInput(userIn)):
                 self.status[param]["val"] = userIn
                 print(self.status[param]["val"])
+                return
+            else:
+                print("Incorrect entry, please try again") 
+        
 
     def checkInput(self, input):
         for keys in self.status: 
@@ -45,22 +55,18 @@ class Sensor:
         else: 
             return False 
         
+    def evaluate(self):
+        input_tuple = (self.status["Light"]["val"], self.status["Person"]["val"], self.status["Vehicle"]["val"])
+        for condition in self.rules:
+            if(input_tuple==condition):
+                return self.rules[input_tuple]
+        return "Stay"
 
 
-
-# The sensor object should be passed to this function to print the action message and current status
-# Replace these comments with your function commenting
-def print_message(sensor):
-    pass
-
-
-
-# Complete the main function below
-def main():
-    print("\n***ENSF 692 Car Vision Detector Processing Program***\n")
-    sens = Sensor()
-    print(sens.status["Person"]["Options"])
+        
+def userInterface(sens: Sensor):
     while(True):
+        print(f"Please select an option from {sens.inputOptions}")
         userIn = input()
         if(sens.checkInput(userIn)): 
             match userIn: 
@@ -69,17 +75,44 @@ def main():
                 case "1": 
                     #option to change light color (Red, Green, Yellow)
                     sens.update_status("Light")
-
+                    print_message("Light", sens)
                 case "2": 
                     #option to change pedestrain status (Yes or No) 
-                    x = 1
+                    sens.update_status("Person")
+                    print_message("Person", sens)
 
                 case "3": 
                     #Option to change car status (Yes or No)
-                    x = 1
+                    sens.update_status("Vehicle")
+                    print_message("Vehicle", sens)
         else:
             print("Input did not match possible options")
+        print(f"\n")
 
+
+
+        
+
+
+
+# The sensor object should be passed to this function to print the action message and current status
+# Replace these comments with your function commenting
+def print_message(action, sensor: Sensor):
+    print(f"The paramter {action} was changed to {sensor.status[action]["val"]}")
+    print(f"Light = {sensor.status["Light"]["val"]}, Person = {sensor.status["Person"]["val"]}, Vehicle = {sensor.status["Vehicle"]["val"]} ")
+    print(sensor.evaluate())
+    
+
+
+
+# Complete the main function below
+def main():
+    print("\n***ENSF 692 Car Vision Detector Processing Program***\n")
+    sens = Sensor()
+    userInterface(sens)
+
+    
+    
 
     
 
